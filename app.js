142e38c09d44d400829437017695027d430a945a
Danil Sherbakov <dandiaborn@gmail.com>
2024-10-24 11:48:34 +0300

    test

--- app.js
+++ app.js
@@ -1,42 +1,42 @@
 const express = require('express');
-const cors = require('cors');
-const { getSubtitles } = require('youtube-captions-scraper');
+// const cors = require('cors');
+// const { getSubtitles } = require('youtube-captions-scraper');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 // Middleware для обработки JSON и CORS
-app.use(express.json());
-app.use(cors());
+// app.use(express.json());
+// app.use(cors());
 
-// Функция для извлечения субтитров с приоритетом русского языка
-async function fetchSubtitlesWithPriority(videoId) {
-  try {
-    // Сначала пытаемся получить субтитры на русском языке
-    let subtitles = await getSubtitles({
-      videoID: videoId,
-      lang: 'ru', // Приоритетный язык - русский
-    });
+// // Функция для извлечения субтитров с приоритетом русского языка
+// async function fetchSubtitlesWithPriority(videoId) {
+//   try {
+//     // Сначала пытаемся получить субтитры на русском языке
+//     let subtitles = await getSubtitles({
+//       videoID: videoId,
+//       lang: 'ru', // Приоритетный язык - русский
+//     });
 
-    // Если субтитров на русском нет, пробуем получить на английском
-    if (!subtitles || subtitles.length === 0) {
-      subtitles = await getSubtitles({
-        videoID: videoId,
-        lang: 'en', // Запасной язык - английский
-      });
-    }
+//     // Если субтитров на русском нет, пробуем получить на английском
+//     if (!subtitles || subtitles.length === 0) {
+//       subtitles = await getSubtitles({
+//         videoID: videoId,
+//         lang: 'en', // Запасной язык - английский
+//       });
+//     }
 
-    // Если субтитры найдены, объединяем их в цельный текст
-    if (subtitles && subtitles.length > 0) {
-      const formattedText = subtitles.map(sub => sub.text).join(' ');
-      return formattedText;
-    } else {
-      return 'No subtitles available';
-    }
-  } catch (error) {
-    return { error: `Error fetching subtitles for video ID ${videoId}: ${error.message}` };
-  }
-}
+//     // Если субтитры найдены, объединяем их в цельный текст
+//     if (subtitles && subtitles.length > 0) {
+//       const formattedText = subtitles.map(sub => sub.text).join(' ');
+//       return formattedText;
+//     } else {
+//       return 'No subtitles available';
+//     }
+//   } catch (error) {
+//     return { error: `Error fetching subtitles for video ID ${videoId}: ${error.message}` };
+//   }
+// }
 
 app.get('/', async (req, res) => {
 
@@ -45,20 +45,20 @@ app.get('/', async (req, res) => {
 });
 
 // POST endpoint для получения субтитров
-app.post('/captions', async (req, res) => {
-  const { videoIds } = req.body;
+// app.post('/captions', async (req, res) => {
+//   const { videoIds } = req.body;
 
-  if (!Array.isArray(videoIds)) {
-    return res.status(400).json({ error: 'Invalid input. Expected an array of video IDs.' });
-  }
+//   if (!Array.isArray(videoIds)) {
+//     return res.status(400).json({ error: 'Invalid input. Expected an array of video IDs.' });
+//   }
 
-  const results = await Promise.all(videoIds.map(async (id) => {
-    const subtitlesText = await fetchSubtitlesWithPriority(id);
-    return { videoId: id, subtitlesText };
-  }));
+//   const results = await Promise.all(videoIds.map(async (id) => {
+//     const subtitlesText = await fetchSubtitlesWithPriority(id);
+//     return { videoId: id, subtitlesText };
+//   }));
 
-  res.json(results);
-});
+//   res.json(results);
+// });
 
 // Запуск сервера
 app.listen(port, () => {
