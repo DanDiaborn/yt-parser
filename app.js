cfdfbdf081f5c19ec8465ad8e4e1bd98b1158b4e
Danil Sherbakov <dandiaborn@gmail.com>
2024-10-28 18:38:27 +0300

    proxy test

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const cors = require('cors');
-const puppeteer = require('puppeteer');
 const { getSubtitles } = require('youtube-captions-scraper');
 const randomUserAgent = require('random-useragent');
+const axios = require('axios');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -10,8 +10,39 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cors());
 
+// Прокси-настройки
+const proxyHost = '93.190.142.57';
+const proxyPort = '9999';
+const proxyUsername = 'ihu31wfnsg-corp-country-PL-state-858787-city-756135-hold-session-session-671faadc61892';
+const proxyPassword = 'hsXWenfhfCjDwacq';
+
+// Настройка прокси через axios
+const axiosInstance = axios.create({
+  proxy: {
+    host: proxyHost,
+    port: parseInt(proxyPort),
+    auth: {
+      username: proxyUsername,
+      password: proxyPassword,
+    },
+  },
+  headers: {
+    'User-Agent': randomUserAgent.getRandom(),
+  },
+});
+
+// Маршрут для проверки IP
+app.get('/check-ip', async (req, res) => {
+  try {
+    const response = await axiosInstance.get('http://api.ipify.org?format=json');
+    res.json({ proxyIP: response.data.ip });
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to fetch IP through proxy', details: error.message });
+  }
+});
+
 async function fetchSubtitlesAuto(videoId) {
-  const languages = ['auto', 'en', 'ru', 'es', 'fr', 'de', 'id'];
+  const languages = ['auto', 'pl', 'en', 'ru', 'es', 'fr', 'de', 'id'];
   let subtitles = null;
 
   for (const lang of languages) {
@@ -19,7 +50,9 @@ async function fetchSubtitlesAuto(videoId) {
       subtitles = await getSubtitles({
         videoID: videoId,
         lang: lang,
+        axiosInstance,
       });
+
       if (subtitles && subtitles.length > 0) {
         const formattedText = subtitles.map(sub => sub.text).join(' ');
         return formattedText;
@@ -33,89 +66,11 @@ async function fetchSubtitlesAuto(videoId) {
   return 'No subtitles available';
 }
 
-const getInstagramPostData = async (shortcode) => {
-  const url = `https://www.instagram.com/p/${shortcode}/?rnd=${Math.random().toString(36).substr(2, 5)}`;
-
-  const browser = await puppeteer.launch({
-    headless: true,
-    args: ['--no-sandbox', '--disable-setuid-sandbox'],
-    executablePath: puppeteer.executablePath()
-  });
-
-  const page = await browser.newPage();
-  const userAgent = randomUserAgent.getRandom();
-  await page.setUserAgent(userAgent);
-  await page.deleteCookie(...(await page.cookies()));
-
-  try {
-    await page.goto(url, { waitUntil: 'networkidle2', timeout: 30000 });
-
-    // Периодическое ожидание для проверки наличия данных
-    const postData = await page.evaluate(() => {
-      const getElementContent = (selector, attribute) => {
-        const element = document.querySelector(selector);
-        return element ? element.getAttribute(attribute) : null;
-      };
-
-      const titleMatch = getElementContent('meta[property="og:title"]', 'content') || 'Title not available';
-      const title = titleMatch.replace(/^.*?:\s*/, '')
-      const info = getElementContent('meta[property="og:description"]', 'content') || 'Description not available';
-
-      const likesMatch = info.match(/(\d+K?) likes/);
-      const likes = likesMatch ? likesMatch[1] : null;
-
-      // Регулярное выражение для извлечения числа комментариев
-      const commentsMatch = info.match(/(\d+) comments/);
-      const comments = commentsMatch ? commentsMatch[1] : null;
-
-      // Регулярное выражение для извлечения даты (формат: месяц день, год)
-      const dateMatch = info.match(/([A-Za-z]+ \d{1,2}, \d{4})/);
-      const date = dateMatch ? dateMatch[1] : null;
-
-      return { title, likes, comments, date };
-    });
-
-
-    await browser.close();
-    return postData;
-  } catch (error) {
-    console.error('Ошибка:', error.message);
-    await browser.close();
-    return null;
-  }
-};
-
-
-
-const randomTimeout = (min = 500, max = 1500) => new Promise(resolve => setTimeout(resolve, Math.floor(Math.random() * (max - min + 1)) + min));
-
 app.get('/', async (req, res) => {
   res.json('ALIVE');
 });
 
-app.post('/inst', async (req, res) => {
-  //   {"postsIds":[
-  // "DBhh5g4TqUa","DBh0_aayF5i","C9JfE2jyMmN"
-  // ]}
-  const { postsIds } = req.body;
-  if (!Array.isArray(postsIds)) {
-    return res.status(400).json({ error: 'Invalid input. Expected an array of post IDs.' });
-  }
-
-  const results = await Promise.all(postsIds.map(async (id) => {
-    await randomTimeout();
-    const postInfo = await getInstagramPostData(id);
-    return { videoId: id, postInfo };
-  }));
-
-  res.json(results);
-});
-
 app.post('/captions', async (req, res) => {
-  // {
-  //   "videoIds": [
-  //     "tR47BnpvBOM", "sbMOkHeGcug"]
-  // }
   const { videoIds } = req.body;
 
   if (!Array.isArray(videoIds)) {
@@ -131,5 +86,5 @@ app.post('/captions', async (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Server running on http://195.161.68.104:49234`);
+  console.log(`Server running on http://195.161.68.104:${port}`);
 });
