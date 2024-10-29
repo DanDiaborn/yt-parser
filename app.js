58bbe917bcd03c0ea2dfa5b3bb5abf33046283bb
Danil Sherbakov <dandiaborn@gmail.com>
2024-10-29 12:38:44 +0300

    errors test

--- app.js
+++ app.js
@@ -1,74 +1,92 @@
 const express = require('express');
 const cors = require('cors');
 const puppeteer = require('puppeteer');
+// const { getSubtitles } = require('youtube-captions-scraper');
+const { getSubtitles } = require('./scrapper.js');
 const randomUserAgent = require('random-useragent');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Proxy credentials
-const proxyHost = '93.190.142.57';
-const proxyPort = '9999';
-const proxyUsername = 'ihu31wfnsg-corp-country-PL-state-858787-city-756135-hold-session-session-671faadc61892';
-const proxyPassword = 'hsXWenfhfCjDwacq';
-
 app.use(express.json());
 app.use(cors());
 
-const getInstagramPostData = async (shortcode) => {
-  const url = `https://www.instagram.com/p/${shortcode}/?rnd=${Math.random().toString(36).substr(2, 5)}`;
+async function fetchSubtitlesAuto(videoId) {
+  const languages = ['auto', 'en', 'ru', 'es', 'fr', 'de', 'id'];
+  let subtitles = null;
 
-  const browser = await puppeteer.launch({
-    headless: true,
-    args: [
-      '--no-sandbox',
-      '--disable-setuid-sandbox',
-      `--proxy-server=http://${proxyHost}:${proxyPort}`
-    ],
-    executablePath: puppeteer.executablePath()
-  });
-
-  const page = await browser.newPage();
-  const userAgent = randomUserAgent.getRandom();
-  await page.setUserAgent(userAgent);
-  await page.deleteCookie(...(await page.cookies()));
-
-  // Auth for proxy
-  await page.authenticate({ username: proxyUsername, password: proxyPassword });
-
-  try {
-    await page.goto(url, { waitUntil: 'networkidle2', timeout: 30000 });
-
-    const postData = await page.evaluate(() => {
-      const getElementContent = (selector, attribute) => {
-        const element = document.querySelector(selector);
-        return element ? element.getAttribute(attribute) : null;
-      };
-
-      const titleMatch = getElementContent('meta[property="og:title"]', 'content') || 'Title not available';
-      const title = titleMatch.replace(/^.*?:\s*/, '');
-      const info = getElementContent('meta[property="og:description"]', 'content') || 'Description not available';
-
-      const likesMatch = info.match(/(\d+K?) likes/);
-      const likes = likesMatch ? likesMatch[1] : null;
-
-      const commentsMatch = info.match(/(\d+) comments/);
-      const comments = commentsMatch ? commentsMatch[1] : null;
-
-      const dateMatch = info.match(/([A-Za-z]+ \d{1,2}, \d{4})/);
-      const date = dateMatch ? dateMatch[1] : null;
-
-      return { title, likes, comments, date };
-    });
-
-    await browser.close();
-    return postData;
-  } catch (error) {
-    console.error('Ошибка:', error.message);
-    await browser.close();
-    return null;
+  for (const lang of languages) {
+    try {
+      subtitles = await getSubtitles({
+        videoID: videoId,
+        lang: lang,
+      });
+      if (subtitles && subtitles.length > 0) {
+        const formattedText = subtitles.map(sub => sub.text).join(' ');
+        return formattedText;
+      }
+    } catch (error) {
+      console.log(`Error fetching subtitles for video ${videoId} on language ${lang}: ${error.message}`);
+      continue;
+    }
   }
-};
+
+  return 'No subtitles available';
+}
+
+// const getInstagramPostData = async (shortcode) => {
+//   const url = `https://www.instagram.com/p/${shortcode}/?rnd=${Math.random().toString(36).substr(2, 5)}`;
+
+//   const browser = await puppeteer.launch({
+//     headless: true,
+//     args: ['--no-sandbox', '--disable-setuid-sandbox'],
+//     executablePath: puppeteer.executablePath()
+//   });
+
+//   const page = await browser.newPage();
+//   const userAgent = randomUserAgent.getRandom();
+//   await page.setUserAgent(userAgent);
+//   await page.deleteCookie(...(await page.cookies()));
+
+//   try {
+//     await page.goto(url, { waitUntil: 'networkidle2', timeout: 30000 });
+
+//     // Периодическое ожидание для проверки наличия данных
+//     const postData = await page.evaluate(() => {
+//       const getElementContent = (selector, attribute) => {
+//         const element = document.querySelector(selector);
+//         return element ? element.getAttribute(attribute) : null;
+//       };
+
+//       const titleMatch = getElementContent('meta[property="og:title"]', 'content') || 'Title not available';
+//       const title = titleMatch.replace(/^.*?:\s*/, '')
+//       const info = getElementContent('meta[property="og:description"]', 'content') || 'Description not available';
+
+//       const likesMatch = info.match(/(\d+K?) likes/);
+//       const likes = likesMatch ? likesMatch[1] : null;
+
+//       // Регулярное выражение для извлечения числа комментариев
+//       const commentsMatch = info.match(/(\d+) comments/);
+//       const comments = commentsMatch ? commentsMatch[1] : null;
+
+//       // Регулярное выражение для извлечения даты (формат: месяц день, год)
+//       const dateMatch = info.match(/([A-Za-z]+ \d{1,2}, \d{4})/);
+//       const date = dateMatch ? dateMatch[1] : null;
+
+//       return { title, likes, comments, date };
+//     });
+
+
+//     await browser.close();
+//     return postData;
+//   } catch (error) {
+//     console.error('Ошибка:', error.message);
+//     await browser.close();
+//     return null;
+//   }
+// };
+
+
 
 const randomTimeout = (min = 500, max = 1500) => new Promise(resolve => setTimeout(resolve, Math.floor(Math.random() * (max - min + 1)) + min));
 
@@ -76,16 +94,38 @@ app.get('/', async (req, res) => {
   res.json('ALIVE');
 });
 
-app.post('/inst', async (req, res) => {
-  const { postsIds } = req.body;
-  if (!Array.isArray(postsIds)) {
-    return res.status(400).json({ error: 'Invalid input. Expected an array of post IDs.' });
+// app.post('/inst', async (req, res) => {
+//   //   {"postsIds":[
+//   // "DBhh5g4TqUa","DBh0_aayF5i","C9JfE2jyMmN"
+//   // ]}
+//   const { postsIds } = req.body;
+//   if (!Array.isArray(postsIds)) {
+//     return res.status(400).json({ error: 'Invalid input. Expected an array of post IDs.' });
+//   }
+
+//   const results = await Promise.all(postsIds.map(async (id) => {
+//     await randomTimeout();
+//     const postInfo = await getInstagramPostData(id);
+//     return { videoId: id, postInfo };
+//   }));
+
+//   res.json(results);
+// });
+
+app.post('/captions', async (req, res) => {
+  // {
+  //   "videoIds": [
+  //     "tR47BnpvBOM", "sbMOkHeGcug"]
+  // }
+  const { videoIds } = req.body;
+
+  if (!Array.isArray(videoIds)) {
+    return res.status(400).json({ error: 'Invalid input. Expected an array of video IDs.' });
   }
 
-  const results = await Promise.all(postsIds.map(async (id) => {
-    await randomTimeout();
-    const postInfo = await getInstagramPostData(id);
-    return { videoId: id, postInfo };
+  const results = await Promise.all(videoIds.map(async (id) => {
+    const subtitlesText = await fetchSubtitlesAuto(id);
+    return { videoId: id, subtitlesText };
   }));
 
   res.json(results);
---
+++ scrapper.js
@@ -0,0 +1,97 @@
+/* @flow */
+
+const he = require('he');
+const axios = require('axios');
+const { find } = require('lodash');
+const striptags = require('striptags');
+
+const fetchData =
+  typeof fetch === 'function'
+    ? async function fetchData(url) {
+      const response = await fetch(url);
+      return await response.text();
+    }
+    : async function fetchData(url) {
+      const { data } = await axios.get(url);
+      return data;
+    };
+
+async function getSubtitles({
+  videoID,
+  lang = 'en',
+}) {
+  let data;
+  try {
+    data = await fetchData(
+      `https://youtube.com/watch?v=${videoID}`
+    );
+  } catch (error) {
+    throw new Error(`Failed to fetch video data for ${videoID}: ${error.message}`);
+  }
+
+  // * ensure we have access to captions data
+  if (!data.includes('captionTracks'))
+    throw new Error(`Could not find captions for video: ${videoID}`);
+
+  const regex = /"captionTracks":(\[.*?\])/;
+  const [match] = regex.exec(data);
+
+  let captionTracks;
+  try {
+    captionTracks = JSON.parse(`{${match}}`).captionTracks;
+  } catch (error) {
+    throw new Error(`Failed to parse caption tracks for video: ${videoID}. Error: ${error.message}`);
+  }
+
+  const subtitle =
+    find(captionTracks, {
+      vssId: `.${lang}`,
+    }) ||
+    find(captionTracks, {
+      vssId: `a.${lang}`,
+    }) ||
+    find(captionTracks, ({ vssId }) => vssId && vssId.match(`.${lang}`));
+
+  // * ensure we have found the correct subtitle lang
+  if (!subtitle || (subtitle && !subtitle.baseUrl))
+    throw new Error(`Could not find ${lang} captions for ${videoID}`);
+
+  let transcript;
+  try {
+    transcript = await fetchData(subtitle.baseUrl);
+  } catch (error) {
+    throw new Error(`Failed to fetch subtitle data for ${videoID} in ${lang}: ${error.message}`);
+  }
+
+  const lines = transcript
+    .replace('<?xml version="1.0" encoding="utf-8" ?><transcript>', '')
+    .replace('</transcript>', '')
+    .split('</text>')
+    .filter(line => line && line.trim())
+    .map(line => {
+      const startRegex = /start="([\d.]+)"/;
+      const durRegex = /dur="([\d.]+)"/;
+
+      const [, start] = startRegex.exec(line) || [];
+      const [, dur] = durRegex.exec(line) || [];
+
+      const htmlText = line
+        .replace(/<text.+>/, '')
+        .replace(/&amp;/gi, '&')
+        .replace(/<\/?[^>]+(>|$)/g, '');
+
+      const decodedText = he.decode(htmlText);
+      const text = striptags(decodedText);
+
+      return {
+        start,
+        dur,
+        text,
+      };
+    });
+
+  return lines;
+}
+
+
+module.exports = { getSubtitles };
