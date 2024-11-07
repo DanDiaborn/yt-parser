c620162837389e0bec7edfe5195f8bfa56293e3a
Danil Sherbakov <dandiaborn@gmail.com>
2024-11-07 09:36:20 +0300

    path update

--- commentsWorker.js
+++ commentsWorker.js
@@ -33,7 +33,7 @@ async function uploadToStorage(data, destination) {
   // Запускаем проверку подключения
   const { url, author, title } = workerData;
   const safeTitle = title.replace(/[<>:"/\\|?*\x00-\x1F]/g, '_');
-  const storagePath = `comments/${safeTitle}.json`;
+  const storagePath = `${author}/${safeTitle}/${safeTitle}.json`;
 
   const browser = await puppeteer.launch({
     args: [
